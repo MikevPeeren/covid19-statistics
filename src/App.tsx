c0ce801b3250a70d0733c89b423d129ed3b8af0e
MikevPeeren <MikevPeeren@hotmail.com>
2020-03-28 15:23:47 +0100

    Cleaning Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
+// React
 import React from 'react';
-import logo from './logo.svg';
+
+// CSS
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      A website to display up to date COVID-19 statistics.
       </header>
     </div>
   );
