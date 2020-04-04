96b539b505ca0947a9e68aac22d1cdfbeab8bb1e
MikevPeeren <MikevPeeren@hotmail.com>
2020-04-04 14:18:13 +0200

    Api Doc

--- src/api/Covid19Api.ts
+++ src/api/Covid19Api.ts
@@ -4,14 +4,26 @@ import axios from 'axios';
 // Constants
 import { COVID_19_BASE_API_URL } from '../constants/general';
 
+/**
+ * Returns Global Statistics about Covid-19
+ */
 export const getCovid19Statistics = async () => {
   return await axios.get(`${COVID_19_BASE_API_URL}/summary`).then((result) => result.data);
 };
 
+/**
+ * Returns all the Countries that are affected by Covid-19
+ */
 export const getCovid19Countries = async () => {
   return await axios.get(`${COVID_19_BASE_API_URL}/countries`).then((result) => result.data);
 };
 
+/**
+ * Returns the detailed information about a Country based on Status given. (confirmed, deaths, recovered)
+ *
+ * @param {string} country
+ * @param {string} status
+ */
 export const getCovid19StatisticsByCountryAndStatus = async (country: string, status: string) => {
   return await axios
     .get(`${COVID_19_BASE_API_URL}/total/country/${country}/status/${status}`)
